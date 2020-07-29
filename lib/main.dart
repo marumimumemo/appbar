32bbfeabbc1dd52c91bfa8e0c27e3bb8d22f10e6
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-07-29 21:07:57 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: const Text('ナビゲーションバー的な'),
+          backgroundColor: Colors.lightBlueAccent,
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.face, color: Colors.white,),
+            ),
+            IconButton(
+              icon: Icon(Icons.email, color: Colors.white,),
+            ),
+            IconButton(
+              icon: Icon(Icons.favorite, color: Colors.white,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
